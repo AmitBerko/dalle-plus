27cad2e7a33afee8a292d983c0eb4e56da4eb7f0
AmitBerko <amit.berko10@gmail.com>
2023-10-28 17:33:35 +0300

    Massive changes - fixed everything

---
+++ frontend/src/accountsExample.js
@@ -0,0 +1,13 @@
+const accounts = [
+	{
+		auth_cookie: '<Your _U cookie>',
+	},
+	{
+		auth_cookie: '<Your _U cookie>',
+	},
+  // Add more accounts as needed
+]
+
+const apiServer = 'http://<Your api server ip>'
+
+module.exports = { accounts, apiServer }
--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,35 +1,123 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
+import { accounts, apiServer } from '../accounts'
 
 function Homepage() {
 	const [urlArray, setUrlArray] = useState([])
 	const [prompt, setPrompt] = useState('')
 
-  const apiServer = 'http://127.0.0.1:8080'
-
-      useEffect(() => {
-				const eventSource = new EventSource(`${apiServer}/sse`)
-				eventSource.onmessage = (event) => {
-					const data = JSON.parse(event.data)
-					setUrlArray((prevUrlArray) => [...prevUrlArray, ...data.links])
-				}
-
-				return () => {
-					eventSource.close()
-				}
-			}, [])
-
-    const handleGenerate = () => {
-      setUrlArray([])
-			axios
-				.post(`${apiServer}/generate-images`, { prompt })
-				.then(() => {
-					console.log('Image generation started')
+	    const handleGenerate = () => {
+				if (!prompt) return
+				setUrlArray([])
+				accounts.forEach((account) => {
+					axios
+						.post(`${apiServer}/generate-images`, { prompt, account })
+						.then((response) => {
+							console.log(`Image generation started for ${account.auth_cookie.slice(0, 5)}`)
+              let newUrls = response.data[account.auth_cookie]
+              console.log(newUrls)
+              if (!newUrls || newUrls === undefined) {
+                console.log('returning - going outside the function')
+                return
+              }
+								setUrlArray((prevUrlArray) => [...prevUrlArray, ...response.data[account.auth_cookie]])
+						})
+						.catch((error) => {
+							console.error(`Error for ${account.auth_cookie.slice(0, 5)}: ${error}`)
+						})
 				})
-				.catch((error) => {
-					console.error(`Error: ${error}`)
-				})
-		}
+			}
+
+			// useEffect(() => {
+			// 	const eventSource = new EventSource(`${apiServer}/sse`)
+
+			// 	eventSource.onmessage = (event) => {
+			// 		const data = JSON.parse(event.data)
+			// 		if (data.links && data.links.length > 0) {
+			// 			setUrlArray((prevUrlArray) => {
+			// 				// Use a Set to ensure uniqueness
+			// 				const uniqueUrls = new Set([...prevUrlArray, ...data.links])
+			// 				return Array.from(uniqueUrls)
+			// 			})
+			// 		} else {
+			// 			console.error('data.links is not iterable:', data.links)
+			// 		}
+			// 	}
+
+			// 	return () => {
+			// 		eventSource.close()
+			// 	}
+			// }, [])
+
+	// const socket = io('http://127.0.0.1:8080', {
+	// 	transports: ['websocket'], // Specify the transport
+	// 	upgrade: false, // Set upgrade to false to prevent WebSocket issues
+	// 	reconnection: true, // Enable reconnection if the connection is dropped
+	// })
+	//////////////
+	// const [urlArray, setUrlArray] = useState([])
+	// const [prompt, setPrompt] = useState('')
+
+	// const socket = io('127.0.0.1:8080', {
+	// 	transports: ['websocket'],
+	// 	upgrade: false,
+	// 	reconnection: true,
+	// 	readyState: 1,
+	// })
+
+	// useEffect(() => {
+	// 	const handleImageUrls = (data) => {
+	// 		console.log(data)
+	// 		setUrlArray((prevUrlsArray) => [...prevUrlsArray, ...data.links])
+	// 	}
+
+	// 	socket.on('image_urls', handleImageUrls)
+
+	// 	return () => {
+	// 		socket.off('image_urls', handleImageUrls)
+	// 	}
+	// }, [socket])
+
+	// const handleGenerate = () => {
+	// 	if (prompt === '' || prompt === undefined) return
+
+	// 	setUrlArray([])
+	// 	socket.emit('generate-images', { prompt })
+
+	// 	// Close the socket here
+	// 	socket.disconnect()
+	// }
+
+	// const [urlArray, setUrlArray] = useState([])
+	// const [prompt, setPrompt] = useState('')
+
+	// const handleGenerate = async () => {
+	// 	if (prompt === '' || prompt === undefined) return
+
+	// 	// Clear the current URLs
+	// 	setUrlArray([])
+
+	// 	try {
+	// 		// Send a request to the server to generate images using Axios
+	// 		const response = await axios.get(`http://127.0.0.1:8080/get-images`, {
+	// 			params: { prompt }, // Pass the prompt as a query parameter
+	// 		})
+
+	// 		if (response.status === 200) {
+	// 			const data = response.data
+	// 			console.log('Received data from server:', data)
+
+	// 			if (data && data.links) {
+	// 				console.log('Updating urlArray with', data.links)
+	// 				setUrlArray(data.links)
+	// 			}
+	// 		} else {
+	// 			console.error('Failed to retrieve images')
+	// 		}
+	// 	} catch (error) {
+	// 		console.error('An error occurred while fetching images:', error)
+	// 	}
+	// }
 
 	return (
 		<>
@@ -41,12 +129,14 @@ function Homepage() {
 						<div className="col-12">
 							<div className="input-group">
 								<input
+									maxLength="480"
 									type="text"
 									value={prompt}
 									onChange={(event) => setPrompt(event.target.value)}
 									className="form-control fs-6 d-none d-sm-block"
 								></input>
 								<textarea
+									maxLength="480"
 									type="text"
 									value={prompt}
 									onChange={(event) => setPrompt(event.target.value)}
@@ -59,11 +149,14 @@ function Homepage() {
 							</div>
 						</div>
 						<div className="col-12 mt-2 mt-lg-0">
-							<button className="btn btn-primary btn-lg w-100 d-sm-none">Generate</button>
+							<button onClick={handleGenerate} className="btn btn-primary btn-lg w-100 d-sm-none">
+								Generate
+							</button>
 						</div>
 					</div>
 				</div>
 			</section>
+
 			{/* Result images section */}
 			<section>
 				<div className="container-fluid px-2 px-md-5">
