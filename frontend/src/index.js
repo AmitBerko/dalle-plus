ff4234550715bf0910888137e6f182896bacf957
AmitBerko <amit.berko10@gmail.com>
2023-10-31 18:30:53 +0200

    Preparing to add firebase realtime db

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Homepage from './pages/Homepage';
 
+import { initializeApp } from 'firebase/app'
+import { getDatabase } from 'firebase/database'
+import firebaseConfig from './firebaseConfig'
 import './assets/styles.css'
+
+const firebaseApp = initializeApp(firebaseConfig)
+const db = getDatabase(firebaseApp) // Initialize the Realtime Database
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,3 +16,5 @@ root.render(
     <Homepage />
   </React.StrictMode>
 )
+
+export { db }
