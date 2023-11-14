80c79a771a4da2824fd8d21daeb8a54dcdfd4fa5
amitberko <amit.berko10@gmail.com>
2023-11-14 22:38:23 +0200

    Preparing for big changes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,13 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Homepage from './pages/Homepage';
 
-import { initializeApp } from 'firebase/app'
-import { getDatabase } from 'firebase/database'
-import firebaseConfig from './firebaseConfig'
+
 import './assets/styles.css'
 
-const firebaseApp = initializeApp(firebaseConfig)
-const db = getDatabase(firebaseApp) // Initialize the Realtime Database
+
+// const db = getDatabase(firebaseApp) // Initialize the Realtime Database
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -17,4 +15,4 @@ root.render(
   </React.StrictMode>
 )
 
-export { db }
+// export { db }
