4e5ee4f9caeeec725451a4f6a91607ddd99b6057
amitberko <amit.berko10@gmail.com>
2023-11-20 00:24:25 +0200

    Fixed

--- backend/api.py
+++ backend/api.py
@@ -29,23 +29,24 @@ def generate_images():
     data = request.json
     prompt = data.get('prompt')
     account = data.get('account')
+    print(f"account is {account}")
     if not prompt or not account:
         return 'Invalid request data', 400
 
     generate_images_for_account(account, prompt)
-    print(f"account is {account.cookie[:5]} and results is {results}")
+    print(f"account is {account["cookie"][:5]} and results is {results}")
     return results
 
 def generate_images_for_account(account, prompt):
-    image_gen = ImageGen(auth_cookie=account.cookie, auth_cookie_SRCHHPGUSR='your_auth_cookie_SRCHHPGUSR')
+    image_gen = ImageGen(auth_cookie=account["cookie"], auth_cookie_SRCHHPGUSR='your_auth_cookie_SRCHHPGUSR')
     try:
         links = image_gen.get_images(prompt)
-        print(f'Links for {account.cookie[:5]}: {links}')
-        results[account.cookie] = links
+        print(f'Links for {account["cookie"][:5]}: {links}')
+        results[account["cookie"]] = links
     except Exception as e:
         # print(f'Account {account["auth_cookie"][:5]} crashed: {str(e)}')
-        print(f"{account.cookie[:5]} crashed due to: {str(e)[:50]}")
-        results[account.cookie] = None
+        print(f"{account["cookie"][:5]} crashed due to: {str(e)[:50]}")
+        results[account["cookie"]] = None
 
 
 @app.route('/')
