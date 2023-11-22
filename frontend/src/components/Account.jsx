c36dc655edb305afa578a30e5dcc1fcb1bc737c9
amitberko <amit.berko10@gmail.com>
2023-11-22 08:58:48 +0200

    Few changes

--- frontend/src/components/Account.jsx
+++ frontend/src/components/Account.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-function Account({ cookie, timeTillExpire = 'testing', removeAccount }) {
+function Account({ cookie, timeTillExpire = '14 Days', removeAccount }) {
 	return (
-		<div className="d-flex align-items-center bg-secondary rounded my-2 py-1">
-			<div className="col-6 text-truncate">{cookie}</div>
-			<div className="col-4 text-center">{timeTillExpire}</div>
+		<div className="d-flex align-items-center bg-secondary rounded my-3 py-1 px-2">
+			<div className="col-6 text-truncate me-1">{cookie}</div>
+			<div className="col-4 text-center">{}</div>
 			<div className="col-2 justify-content-end d-flex">
-				<i className="bi bi-x fs-4" style={{ cursor: 'pointer' }} onClick={() => removeAccount(cookie)}></i>
+				<i className="bi bi-x fs-4 me-1" style={{ cursor: 'pointer' }} onClick={() => removeAccount(cookie)}></i>
 				{/* <i className="bi bi-pencil-square fs-5" style={{cursor: "pointer"}}></i> */}
 			</div>
 		</div>
--- frontend/src/components/AccountsModal.jsx
+++ frontend/src/components/AccountsModal.jsx
@@ -5,31 +5,31 @@ import { ref, get, onValue, query, update } from 'firebase/database'
 
 function AccountsModal({ userUid, accounts, setAccounts }) {
 	const [cookieInput, setCookieInput] = useState('')
-  const expirationLength = 1000 * 60 * 60 * 24 * 14 // 14 days
+	const expirationLength = 1000 * 60 * 60 * 24 * 14 // 14 days
 
 	const userRef = ref(db, `users/${userUid}`)
 
 	useEffect(() => {
-    // Todo: Maybe combine them later
+		// Todo: Maybe combine them later
 		const getAccounts = async () => {
 			const snapshot = await get(userRef)
 			const accounts = snapshot.val().accounts
 			setAccounts(accounts ? accounts : [])
 		}
 
-    const removeExpiredAccounts = () => {
-      // Each cookie expires after a maximum of 14 days
-      if (!accounts || accounts.length === 0) return
-      const unexpiredAccounts = accounts.filter((account) => {
-        const isExpired = account.creationDate + expirationLength < Date.now()
-        if (!isExpired) return account
-      })
-      setAccounts(unexpiredAccounts)
-      update(userRef, { accounts: unexpiredAccounts })
-    }
+		const removeExpiredAccounts = () => {
+			// Each cookie expires after a maximum of 14 days
+			if (!accounts || accounts.length === 0) return
+			const unexpiredAccounts = accounts.filter((account) => {
+				const isExpired = account.creationDate + expirationLength < Date.now()
+				if (!isExpired) return account
+			})
+			setAccounts(unexpiredAccounts)
+			update(userRef, { accounts: unexpiredAccounts })
+		}
 
 		getAccounts()
-    removeExpiredAccounts()
+		removeExpiredAccounts()
 	}, [])
 
 	function removeAccount(cookie) {
@@ -42,14 +42,15 @@ function AccountsModal({ userUid, accounts, setAccounts }) {
 		if (cookieInput === '') return
 
 		try {
-      const creationDate = Date.now()
-      const match = cookieInput.match(/_U\n(.*?)\n-{3,}/)
-      // Just so I can use an some kind of app that gives me the cookie
-      const cookie = match ? match[1] : cookieInput
+			const creationDate = Date.now()
+			const match = cookieInput.match(/_U\s*(.*?)(?=\s*-{3,})/)
+			// Just so I can use an some kind of app that gives me the cookie
+			const cookie = match ? match[1] : cookieInput
+			console.log(cookie)
 			const newAccounts = [{ cookie: cookie, isGenerating: false, creationDate }, ...(accounts ? accounts : [])]
 			setAccounts(newAccounts)
-      setCookieInput('')
-			update(userRef, { accounts: newAccounts, })
+			setCookieInput('')
+			update(userRef, { accounts: newAccounts })
 		} catch (error) {
 			console.log(`error is ${error}`)
 		}
@@ -67,6 +68,14 @@ function AccountsModal({ userUid, accounts, setAccounts }) {
 		})
 	}
 
+	function getExpiresIn(creationDate) {
+		let expiresIn = creationDate + expirationLength - Date.now() // In milliseconds for now
+		let expiresInDays = parseInt(expiresIn / (1000 * 60 * 60 * 24))
+
+    // Either show days or hours
+		return expiresInDays > 1 ? `${expiresInDays} Days` : `${expiresInDays / 24} Hours`
+	}
+
 	return (
 		<>
 			<div className="modal p-2 py-5 fade" id="accounts-modal" tabIndex="-1">
@@ -77,17 +86,24 @@ function AccountsModal({ userUid, accounts, setAccounts }) {
 							<button className="btn-close" data-bs-dismiss="modal"></button>
 						</div>
 
-						<div className="modal-body px-4 mb-1">
-							<div className="row d-flex align-content-center">
-								<div className="col-6 me-2">Cookie</div>
-								<div className="col-3">asdadasdasda</div>
-
-								{accounts
-									? accounts.map((account, index) => {
-											return <Account key={index} removeAccount={removeAccount} cookie={account.cookie} />
-									  })
-									: null}
+						<div className="modal-body px-3 px-sm-4 mb-1">
+							<div className="row d-flex align-items-center">
+								<div className="col-6 text-center fs-5">Cookie</div>
+								<div className="col-4 fs-5 text-center ps-0 no-wrap">Expires in</div>
 							</div>
+
+							{accounts
+								? accounts.map((account, index) => {
+										return (
+											<Account
+												key={index}
+												removeAccount={removeAccount}
+												timeTillExpire={getExpiresIn(account.creationDate)}
+												cookie={account.cookie}
+											/>
+										)
+								  })
+								: null}
 						</div>
 
 						<div className="modal-footer">
