a28d292c54c59068c8fb404035fb8a0efb0bbf0a
amitberko <amit.berko10@gmail.com>
2023-11-30 16:42:07 +0200

    Removed homepage comment

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Authentication from './components/Authentication'
-import Homepage from './pages/Homepage';
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import './assets/styles.css'
 
@@ -10,6 +9,5 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Authentication />
-    {/* <Homepage /> */}
   </React.StrictMode>
 )
