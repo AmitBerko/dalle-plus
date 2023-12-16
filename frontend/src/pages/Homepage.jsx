7a1b5ccdbe3d8bd60bd26a5cc8ab4789cee5cf2b
amitberko <amit.berko10@gmail.com>
2023-12-16 18:53:09 +0200

    Major changes and cleanup

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,25 +1,27 @@
 import React, { useState, useEffect } from 'react'
 import { db } from '../firebaseConfig'
-import { ref, get, onValue, query, update } from 'firebase/database'
+import { ref, get, onValue, update } from 'firebase/database'
 import AccountsModal from '../components/AccountsModal'
-import io from 'socket.io-client'
 import { useSelector, useDispatch } from 'react-redux'
-import { setAccounts, updateAccount, addAccount } from '../redux/accountsSlice'
+import { setAccounts, updateAccount } from '../redux/accountsSlice'
+import { io } from 'socket.io-client'
 
-const socket = io('https://super-dalle3.onrender.com')
-// const socket = io('localhost:8080')
+// const backendUrl = 'http://localhost:8080'
+const backendUrl = 'https://super-dalle3.onrender.com'
+
+const socket = io(backendUrl)
 
 function Homepage({ setIsLoggedIn, userUid }) {
 	const accounts = useSelector((state) => state.accounts.value)
 	const dispatch = useDispatch()
-	// const { setAccounts, updateAccount, fetchAccounts } = useFirebaseAccounts(userUid)
 	const [urlArray, setUrlArray] = useState([])
 	const [prompt, setPrompt] = useState('')
 	const [generatingCount, setGeneratingCount] = useState(0)
 	const [isSlowMode, setIsSlowMode] = useState(true)
 	const [isGenerating, setIsGenerating] = useState(false)
 	const [user, setUser] = useState(null)
-	const accountsRef = `users/${userUid}/accounts`
+	const accountsRef = ref(db, `users/${userUid}/accounts`)
+	const imagesRef = ref(db, `users/${userUid}/generatedImages`)
 
 	// Getting initial user data
 	useEffect(() => {
@@ -47,30 +49,28 @@ function Homepage({ setIsLoggedIn, userUid }) {
 		updateDb()
 		const generatingAccounts = accounts.filter((account) => account.isGenerating)
 		setGeneratingCount(generatingAccounts.length)
-	}, [accounts])
+		}, [accounts])
+
+	useEffect(() => {
+		const unsubscribe = onValue(imagesRef, (snapshot) => {
+			const data = snapshot.val()
+			setUrlArray(data)
+			console.log(`the unsub data is: `, data)
+		})
+
+		return () => unsubscribe()
+	}, [userUid])
 
 	useEffect(() => {
 		socket.on('connect', () => {
 			console.log('client connect')
 		})
 
-		socket.on('imagesGenerated', (data) => {
-			console.log(`data is`, data)
-			const urls = data.urls
+		socket.on('finishedGeneration', (data) => {
 			const cookie = data.cookie
 			const accountIndex = accounts.findIndex((account) => account.cookie === cookie)
 			dispatch(updateAccount({ accountIndex, newValues: { isGenerating: false } }))
 			setIsGenerating(false)
-			if (!urls) return
-			// console.log(`${cookie.slice(0, 5)} generated`)
-			setUrlArray((prevUrls) => prevUrls.concat(urls))
-			console.log(`the cookie is ${cookie.slice(0, 5)} and index is ${accountIndex}`)
-		})
-
-		socket.on('imagesFailed', (data) => {
-			const cookie = data.cookie
-			const accountIndex = accounts.findIndex((account) => account.cookie === cookie)
-			dispatch(updateAccount({ accountIndex, newValues: { isGenerating: false } }))
 		})
 
 		return () => {
@@ -86,58 +86,15 @@ function Homepage({ setIsLoggedIn, userUid }) {
 		}
 	}, [])
 
-	// useEffect(() => {
-	//   const filteredUrls = urlArray.filter((image) => !errorImages.includes(image))
-	//   // Set isGenerating to true either if there's a good image, or 50% of accounts has finished generating
-	//   if (filteredUrls.length > 0) setIsGenerating(false)
-	//   if (generatingCount <= parseInt(accounts.length * 0.5)) setIsGenerating(false)
-	//   // if (generatingCount === 0) setIsGenerating(false)
-	//   setFilteredUrlArray(filteredUrls)
-	// }, [urlArray])
-
 	const handleGenerate = () => {
 		if (!prompt) return
-		setUrlArray([])
-		setIsGenerating(true)
-		socket.emit('generateImages', { prompt, accounts, isSlowMode })
+    const notGeneratingAccounts = accounts.filter(account => !account.isGenerating)
+    console.log(`notGeneratingAccounts are`, notGeneratingAccounts)
+		socket.emit('generateImages', { prompt, accounts: notGeneratingAccounts, isSlowMode, userUid })
 		const updatedAccounts = accounts.map((account) => {
 			return { ...account, isGenerating: true }
 		})
 		dispatch(setAccounts(updatedAccounts))
-		// // Set every account's isGenerating status to true
-		// // setAccounts((prevAccounts) => {
-		// //   const result = prevAccounts.map((account) => {
-		// //     return { ...account, isGenerating: true }
-		// //   })
-		// //   console.log(result)
-		// //   return result
-		// // })
-		// const userRef = ref(db, `users/${userUid}`)
-		// update(userRef, { accounts })
-		// setGeneratingCount(accounts.length)
-	}
-
-	function updateGeneratingStatus(cookie, isGenerating) {
-		// if (cookie === undefined || cookie === '') return
-		// const accountIndex = accounts.findIndex((acc) => acc.cookie === cookie)
-		// if (accountIndex === -1) {
-		// 	console.log('Account not found')
-		// 	return
-		// }
-		// const updatedAccountRef = ref(db, `${accountsRef}/${accountIndex}`)
-		// // Update isGenerating value on firebase and accounts state
-		// // setAccounts((prevAccounts) => {
-		// //   prevAccounts[accountIndex].isGenerating = isGenerating
-		// //   return prevAccounts
-		// // })
-		// update(updatedAccountRef, { cookie, isGenerating })
-	}
-
-	function getIsGenerating(cookie) {
-		const account = accounts.find((acc) => acc.cookie === cookie)
-		if (!account) return
-		console.log(`account:`, account, `s isgenerating is: ${account.isGenerating}`)
-		return account.isGenerating
 	}
 
 	// Set all accounts' isGenerating to false when exiting a page
@@ -146,24 +103,13 @@ function Homepage({ setIsLoggedIn, userUid }) {
 			return { ...account, isGenerating: false }
 		})
 
-    dispatch(setAccounts(updatedAccounts))
+		dispatch(setAccounts(updatedAccounts))
+		clearImages()
 	}
 
-	function printUser(userUid) {
-		const userQuery = query(ref(db, `users/${userUid}`))
-
-		onValue(userQuery, (snapshot) => {
-			if (!snapshot.exists) {
-				console.log('snapshot doesnt exist')
-				return
-			}
-			console.log(snapshot.val())
-		})
-	}
-
-	function testing() {
-		dispatch(updateAccount({ accountIndex: 0, newValues: { isGenerating: false } }))
-	}
+  function clearImages() {
+    socket.emit('clearImages', { userUid })
+  }
 
 	return (
 		<>
@@ -245,7 +191,9 @@ function Homepage({ setIsLoggedIn, userUid }) {
 				{/* Add account - Big */}
 				<div className="row mt-3 mb-2 d-none d-md-flex">
 					<div className="col">
-						<button className="btn btn-danger d-none w-100 d-md-block">Ping Api Servers</button>
+						<button className="btn btn-danger d-none w-100 d-md-block" onClick={clearImages}>
+							Clear Images
+						</button>
 					</div>
 					<div className="col">
 						<button
@@ -272,7 +220,9 @@ function Homepage({ setIsLoggedIn, userUid }) {
 				{/* Accounts / images / Checkbox */}
 				<div className="row">
 					<div className="col-xl-4 col-md-6 mb-md-2 d-flex justify-content-center justify-content-md-end justify-content-xl-center">
-						<div className="fs-3 text-center">Successful images: {urlArray.length}</div>
+						<div className="fs-3 text-center">
+							Successful images: {urlArray ? urlArray.length : 0}
+						</div>
 					</div>
 					<div className="col-xl-4 col-md-6 d-flex justify-content-center justify-content-md-start justify-content-xl-center">
 						<div className="fs-3 text-center">
@@ -304,63 +254,20 @@ function Homepage({ setIsLoggedIn, userUid }) {
 			<section>
 				<div className="container-fluid px-2 px-md-5">
 					<div className="row justify-content-center">
-						{urlArray.map((url, index) => {
-							return (
-								<img
-									key={index}
-									src={`${url}?w=270&h=270`}
-									alt={url}
-									className="img-fluid generated-image p-2 m-0"
-									onClick={() => window.open(url, '_blank')}
-									style={{ cursor: 'pointer' }}
-								/>
-							)
-						})}
+						{urlArray?.map((url, index) => (
+							<img
+								key={index}
+								src={`${url}?w=270&h=270`}
+								alt={url}
+								className="img-fluid generated-image p-2 m-0"
+								onClick={() => window.open(url, '_blank')}
+								style={{ cursor: 'pointer' }}
+							/>
+						))}
 					</div>
 				</div>
 			</section>
-
 			<AccountsModal userUid={userUid} />
-
-			{/* <div>
-				<button className="btn btn-warning" data-bs-toggle="modal" data-bs-target="#accounts-modal">
-					open modal
-				</button>
-			</div> */}
-			{/* <button
-				onClick={() => {
-					console.log(accounts)
-				}}
-			>
-				print accounts
-			</button> */}
-
-			{/* <button
-        onClick={() =>
-          updateGeneratingStatus(
-            '1GUivhY-8ozgPkE1kL1lKVQpklGbylujaaDcEuMViTBEykuVBVHWEcg9KyhuQCSWPVNIU1_ksTfYzNdmeHIso3XJIJFOcZFuOhZaVHxWChq-JkRuE2lIjIBOaszRtF6eyvWyAi1uzQG8lXg3ayAXdepUBnLo36UOPoQxlLhOlus54B9pcd0tAMA6orNe7I5KOoKNMsiOD1JHJipJ2ONJTGg',
-            false
-          )
-        }
-      >
-        change user
-      </button> */}
-
-			{/* <button onClick={testing}>test button</button>
-			<button
-				onClick={() =>
-					dispatch(
-						addAccount({
-							cookie: Math.floor(Math.random() * 1000),
-							isGenerating: false,
-							creationDate: '123',
-						})
-					)
-				}
-			>
-				test Redux
-			</button> */}
-			{/* <h6 className="display-6">{JSON.stringify(accounts)}</h6> */}
 		</>
 	)
 }
