1e3dd3153dc4611c27cc87e8945efb34dfeff309
amitberko <amit.berko10@gmail.com>
2023-12-23 22:36:53 +0200

    Removed unnecessary lines

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,7 +6,6 @@ import accountsReducer from './redux/accountsSlice'
 import { configureStore } from '@reduxjs/toolkit'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import './assets/styles.css'
-import Homepage from './pages/Homepage'
 
 const store = configureStore({
 	reducer: {
@@ -18,7 +17,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
 	<React.StrictMode>
 		<Provider store={store}>
-			{/* <Homepage userUid={'zwC3U9vXq1dzDvGBM1exS4VwTzI3'} /> */}
 			<Authentication />
 		</Provider>
 	</React.StrictMode>
